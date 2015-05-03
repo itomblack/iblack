511ad9ec29f2f228554da48440bd47ebace84213
itomblack <itomblack@Ians-MBP.lan>
2015-05-03 10:46:25 +0100

    new JS

--- src/js/main.js
+++ src/js/main.js
@@ -9,19 +9,52 @@ $( document ).ready(function() {
   'blog'
   ];
 
-  //load menu & footer
+  //LOAD MENU AND FOOTER
   $('header').load('menu.html');
   $('footer').load('footer.html');
 
-  
+  //************** EVENT LISTENERS ***************//
 
-  // Define Drawing function for homepage
-  var drawingPaths = function (){
-  
+  $('#content-prev').click(function(){
+    moveSection(999, -1); //999 means not a direct project link
+  });
+
+  $('#content-next').click(function(){
+    moveSection(999, 1); //999 means not a direct project link
+  });
+
+  $('#content-nav').click(function(){
+    var projectId = event.target.id.slice(-1);
+    moveSection(projectId, 0);
+
+  });
+
+
+  // ********** IE test ******** //
+
+  function isIE () {
+    var myNav = navigator.userAgent.toLowerCase();
+    return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+  }
+
+  if (isIE () < 9) {
+   // is IE version less than 9
+  } else {
+   // is IE 9 and later or not IE
+  }
+
+
+
+  // ********** HOMEPAGE DRAWING FUNCTION ******** //
+
+  function drawingPaths(){
+    console.log('run');
     var current_frame = 0;
     var total_frames = 90;
     var path = [];
     var length = [];
+
+
 
     path = document.querySelectorAll('path');
 
@@ -47,75 +80,84 @@ $( document ).ready(function() {
        }
     };
     draw();
-  };
+  }
 
 
-// load svgs into home page <a> tags
-for (i=0; i<menuItems.length; i++) {
-  $( "#home-ill-" + menuItems[i] ).load("img/home-svgs/" + menuItems[i] + ".svg", drawingPaths);
+// ************ LOAD SVGS INTO HOME PAGE ********** //
+function loadHomeImages (){
+  var count = 1;
+  for (i=0; i<menuItems.length; i++) {
+    $( "#home-ill-" + menuItems[i] ).load("img/home-svgs/" + menuItems[i] + ".svg", function(){
+      //run drawing function after final image is done loading
+      count++;
+      if (count == menuItems.length) {
+        drawingPaths();
+      }
+    });
+  }
 }
 
+loadHomeImages();
 
-//*********** scrolling between projects ********//
 
-$('#content-prev').click(function(){
-  imgSwap(-1);
-  moveSection(-1);
-});
-$('#content-next').click(function(){
-  imgSwap(1);
-  moveSection(1);
-});
 
-function moveSection(direction) {
+
+
+
+//*********** SCROLLING BETWEEN PROJECTS ********//
+
+function moveSection(search, direction) {
   var contentPara = $('.content-para'); 
   var listLength = contentPara.length;
-  for(var i=0; i<listLength; i++){
+  var visibleContent = "";
+  var nextContent = "";
 
-    if ( $(contentPara[i]).hasClass('content-visible') === true ) {
-      $(contentPara[i]).removeClass('content-visible');
-
-      //if at start of list go to end
-      if ( (i + direction) < 0 ) { i = listLength; }
-
-      //if at end of list go to start
-      if ( (i + direction) >= listLength ) { i = -1; }
-      
-      $(contentPara[i + direction]).addClass('content-visible');
-
-      i = listLength; //exit loop
-    }
-  }
-} //*************** END MOVESECTION ************//
-
-function imgSwap(direction) {
-  var contentPara = $('.content-para'); 
-  var listLength = contentPara.length;
+ 
   //get current visible section
-  var visibleContent = $('.content-visible').attr('id').slice(-1);
+  visibleContent = $('.content-visible').attr('id').slice(-1);
   visibleContent = parseInt(visibleContent);
+  
+
+  //toggle visible class on paragraph
+  $(contentPara[visibleContent]).toggleClass('content-visible');
+
   //if intro section then swap svgs
   if ( visibleContent === 0) {
     $('#content-img-intro').toggleClass('wrap-hide');
     $('[id^=content-img-wrap]').toggleClass('wrap-hide');
   }
-  //remove image background from others
-  $('[id^=content-img-view]').removeClass('content-view-' + visibleContent); 
 
-  //work out next to be shown - if intro then swap svgs
-    //if at start of list go to end
-    if ( (visibleContent + direction) < 0 ) { visibleContent = listLength; }
-    //if at end of list go to start
-    if ( (visibleContent + direction) >= listLength ) { visibleContent = -1; }
-    var nextContent = visibleContent + direction;
-    if ( nextContent === 0 ) { 
-      $('#content-img-intro').toggleClass('wrap-hide');
-      $('[id^=content-img-wrap]').toggleClass('wrap-hide');
-     }
+  //remove image background from others
+  $('[id^=content-img-view]').removeClass('content-view-' + visibleContent);
+
+
+  //find next to be shown
+  if (search === 999 ) {
+      //if at start of list go to end
+      if ( (visibleContent + direction) < 0 ) { visibleContent = listLength; }
+      //if at end of list go to start
+      if ( (visibleContent + direction) >= listLength ) { visibleContent = -1; }
+      nextContent = visibleContent + direction;
+  }
+  else {
+    nextContent = search;
+  }
+
+  //toggle visible class for next content paragraph
+  $(contentPara[nextContent]).toggleClass('content-visible');
+
+  //if next section is intro section then swap svgs
+  if ( nextContent === 0 ) { 
+    $('#content-img-intro').toggleClass('wrap-hide');
+    $('[id^=content-img-wrap]').toggleClass('wrap-hide');
+   }
   //if not intro load new images
   $('[id^=content-img-view]').addClass('content-view-' + nextContent);
-}
 
+
+
+
+} //*************** END MOVESECTION ************//
 
 
 
