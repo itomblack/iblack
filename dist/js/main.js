4d5bd6e8e40b5f042da4c7dc8be511f8c9bb2c5b
itomblack <itomblack@hotmail.co.uk>
2016-03-06 19:05:30 +0000

    new photography bits

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,19 +9,12 @@ $( document ).ready(function() {
   'blog'
   ];
 
-  //LOAD MENU AND FOOTER
-  // $('header').load('menu.html');
-  // $('footer').load('footer.html');
-
-
 
   //************** DESIGN SECTION ***************//
 
   $(".project-title").click(function () {
-    // $(".js-project-open").removeClass('js-project-open');
     $(this).toggleClass('js-project-open');
     $(this).next().toggleClass('js-project-open');
-
   })
 
 
@@ -30,211 +23,236 @@ $( document ).ready(function() {
 
   // ********** IE test ******** //
 
-  function isIE () {
-    var myNav = navigator.userAgent.toLowerCase();
-    return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
-  }
+  // function isIE () {
+  //   var myNav = navigator.userAgent.toLowerCase();
+  //   return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
+  // }
 
-  if (isIE () < 9) {
-   // is IE version less than 9
-  } else {
-   // is IE 9 and later or not IE
-  }
+  // if (isIE () < 9) {
+  //  // is IE version less than 9
+  // } else {
+  //  // is IE 9 and later or not IE
+  // }
 
 
 
   // ********** HOMEPAGE DRAWING FUNCTION ******** //
+  var homeFeatures  = function () {
 
-  function drawingPaths(){
-    var current_frame = 0;
-    var total_frames = 90;
-    var path = [];
-    var length = [];
-
-
-
-    path = document.querySelectorAll('path');
-
-    for(var i=0; i<path.length;i++){
-
-      l = path[i].getTotalLength();
-      length[i] = l;
-      path[i].style.strokeDasharray = l + ' ' + l; 
-      path[i].style.strokeDashoffset = l;
-    }
-    var handle = 0;
-
-    var draw = function() {
-       var progress = current_frame/total_frames;
-       if (progress > 1) {
-         window.cancelAnimationFrame(handle);
-       } else {
-         current_frame++;
-         for(var j=0; j<path.length;j++){
-           path[j].style.strokeDashoffset = Math.floor(length[j] * (1 - progress));
-         }
-         handle = window.requestAnimationFrame(draw);
-       }
-    };
-    draw();
-  }
+      function drawingPaths(){
+        var current_frame = 0;
+        var total_frames = 90;
+        var path = [];
+        var length = [];
 
 
-// ************ LOAD SVGS INTO HOME PAGE ********** //
-function loadHomeImages (){
-  var count = 1;
-  for (i=0; i<menuItems.length; i++) {
-    $( "#home-ill-" + menuItems[i] ).load("img/home-svgs/" + menuItems[i] + ".svg", function(){
-      //run drawing function after final image is done loading
-      count++;
-      if (count == menuItems.length) {
-        drawingPaths();
+
+        path = document.querySelectorAll('path');
+
+        for(var i=0; i<path.length;i++){
+
+          l = path[i].getTotalLength();
+          length[i] = l;
+          path[i].style.strokeDasharray = l + ' ' + l; 
+          path[i].style.strokeDashoffset = l;
+        }
+        var handle = 0;
+
+        var draw = function() {
+           var progress = current_frame/total_frames;
+           if (progress > 1) {
+             window.cancelAnimationFrame(handle);
+           } else {
+             current_frame++;
+             for(var j=0; j<path.length;j++){
+               path[j].style.strokeDashoffset = Math.floor(length[j] * (1 - progress));
+             }
+             handle = window.requestAnimationFrame(draw);
+           }
+        };
+        draw();
       }
-    });
-  }
+
+
+    // ************ LOAD SVGS INTO HOME PAGE ********** //
+    function loadHomeImages (){
+      var count = 1;
+      for (i=0; i<menuItems.length; i++) {
+        $( "#home-ill-" + menuItems[i] ).load("img/home-svgs/" + menuItems[i] + ".svg", function(){
+          //run drawing function after final image is done loading
+          count++;
+          if (count == menuItems.length) {
+            drawingPaths();
+          }
+        });
+      }
+    }
+
+    loadHomeImages();
 }
 
-loadHomeImages();
+// run if on HOME page
+if ( $('#home-body').length ) { 
+  homeFeatures();
+}
 
 
 
 //***************** ILLUSTRATION SHOW ***************//
 //***************************************************//
 
-var illustration = $('.ill-thumb');
+var illustrationFeatures  = function () {
 
-//clicking an image shows large image
-illustration.click(function(){
-  
-  //get clicked image and load full version
-  var thumbUrl = $(this).attr('src');
-  var fullUrl = thumbUrl.replace("thumbs", "full");
+    var illustration = $('.ill-thumb');
 
-
-
-  $('#ill-full').fadeOut(200, function(){
-        $(this).attr('src', fullUrl).bind('onreadystatechange load', function(){
-              if (this.complete) $(this).fadeIn(500);
-        });
-  });   
+    //clicking an image shows large image
+    illustration.click(function(){
+      
+      //get clicked image and load full version
+      var thumbUrl = $(this).attr('src');
+      var fullUrl = thumbUrl.replace("thumbs", "full");
 
 
-  //show page
-  var imageBack = $('#full-image-back');
-  imageBack.toggleClass('js-on-page');
-});
+
+      $('#ill-full').fadeOut(200, function(){
+            $(this).attr('src', fullUrl).bind('onreadystatechange load', function(){
+                  if (this.complete) $(this).fadeIn(500);
+            });
+      });   
 
 
-//clicking x button hides large image
-
-$('#full-image-close').click(function(){
-  var imageBack = $('#full-image-back');
-  imageBack.toggleClass('js-on-page');
-});
+      //show page
+      var imageBack = $('#full-image-back');
+      imageBack.toggleClass('js-on-page');
+    });
 
 
-//FILTER
+    //clicking x button hides large image
 
-$('[id*=filter]').click( function() {
-  var filter = this.id.split('-')[1];
-  var allPics = $('.ill-thumb');
-  //if off then remove class and show images
-  if ($(this).hasClass('filter-on')) {
-      $(this).removeClass('filter-on');
-      for (i=0; i<allPics.length; i++) {
-          if ($(allPics[i]).attr('data').indexOf(filter) == 0) {
-            $(allPics[i]).removeClass('hide-illustration');
-          };
+    $('#full-image-close').click(function(){
+      var imageBack = $('#full-image-back');
+      imageBack.toggleClass('js-on-page');
+    });
+
+
+    //FILTER
+
+    $('[id*=filter]').click( function() {
+      var filter = this.id.split('-')[1];
+      var allPics = $('.ill-thumb');
+      //if off then remove class and show images
+      if ($(this).hasClass('filter-on')) {
+          $(this).removeClass('filter-on');
+          for (i=0; i<allPics.length; i++) {
+              if ($(allPics[i]).attr('data').indexOf(filter) == 0) {
+                $(allPics[i]).removeClass('hide-illustration');
+              };
+          }
+
+      } else {
+          // else add class and hide images
+          $(this).addClass('filter-on');
+
+          // $('[data=' + filter + ']').addClass('hide-illustration')
+
+          for (i=0; i<allPics.length; i++) {
+              if ($(allPics[i]).attr('data').indexOf(filter) == 0) {
+                $(allPics[i]).addClass('hide-illustration');
+              };
+          }
+
       }
+    });
+} /* END ILLUSTRATION FEATURES */
 
-  } else {
-      // else add class and hide images
-      $(this).addClass('filter-on');
-
-      // $('[data=' + filter + ']').addClass('hide-illustration')
-
-      for (i=0; i<allPics.length; i++) {
-          if ($(allPics[i]).attr('data').indexOf(filter) == 0) {
-            $(allPics[i]).addClass('hide-illustration');
-          };
-      }
-
-  }
-});
+// run if on ILLUSTRATION page
+if ( $('#illustration-body').length ) { 
+  illustrationFeatures();
+}
 
 
 
 //**************** PHOTOGRAHY SHOW ******************//
 //***************************************************//
 
+var photographyFeatures  = function () {
 
+    // define max num of images
+    var imgTotal;
 
-// define max num of images
-var imgTotal = 21;
-
-
-
-
-// if you hit button then get direction and call function
-$('[id*=photo-swap-]').click( function(event) {
-  if ($(this).attr('id').indexOf('left') > -1 ) {
-    movePhoto(-1);  
-  }
-  else if ($(this).attr('id').indexOf('right') > -1 ) {
-    movePhoto(1);  
-  }
-});
-
-// if arrows clicked get direction and call function
-document.onkeydown = function(e) {
-    switch (e.keyCode) {
-
-        case 37: //left
-          if ($('body').attr('id') == 'photog-body') {
-            movePhoto(-1);
-          };  
-          break;
-
-        case 39: //right
-          if ($('body').attr('id') == 'photog-body') {
-            movePhoto(1);
-          };  
-          break;
+    if ( $('#main-photo').attr('class').indexOf('rome-21') >=0 ) {
+      imgTotal = 21;
+    } else if ( $('#main-photo').attr('class').indexOf('nz-32') >=0 ) {
+      imgTotal = 32;
     }
-};
 
 
 
-// move to next image based on direction
-var movePhoto = function(direction) {
+    // if you hit button then get direction and call function
+    $('[id*=photo-swap-]').click( function(event) {
+      if ($(this).attr('id').indexOf('left') > -1 ) {
+        movePhoto(-1);  
+      }
+      else if ($(this).attr('id').indexOf('right') > -1 ) {
+        movePhoto(1);  
+      }
+    });
 
-  //get existing image number
-  var currentNum = parseInt($('#main-photo').attr('src')
-  .replace('img/rome-web/rome-', '')
-  .replace('.jpg', '')
-  );
+    // if arrows clicked get direction and call function
+    document.onkeydown = function(e) {
+        switch (e.keyCode) {
 
-  //get next image number
-  var nextNum = currentNum + direction;
+            case 37: //left
+              if ($('body').attr('id') == 'photog-body') {
+                movePhoto(-1);
+              };  
+              break;
 
-  //loop at start and end of number of images
-  if (nextNum < 1) {
-    nextNum = imgTotal;
-  } else if (nextNum > imgTotal) {
-    nextNum = 1;
-  }
-
-  // select image and swap out for next one
-  $('#main-photo').fadeOut(500, function(){
-      $(this).attr('src', 'img/rome-web/rome-' + nextNum + '.jpg' ).bind('onreadystatechange load', function(){
-         if (this.complete) $(this).fadeIn(500);
-      });
-  });   
-};
+            case 39: //right
+              if ($('body').attr('id') == 'photog-body') {
+                movePhoto(1);
+              };  
+              break;
+        }
+    };
 
 
 
+    // move to next image based on direction
+    var movePhoto = function(direction) {
 
+      //get existing image number
+      var currentNum = parseInt($('#main-photo').attr('src')
+      .replace('img/rome-web/rome-', '')
+      .replace('img/nz-web/nz-', '')
+      .replace('.jpg', '')
+      );
+
+      //get next image number
+      var nextNum = currentNum + direction;
+
+      //loop at start and end of number of images
+      if (nextNum < 1) {
+        nextNum = imgTotal;
+      } else if (nextNum > imgTotal) {
+        nextNum = 1;
+      }
+
+      // select image and swap out for next one
+      $('#main-photo').fadeOut(500, function(){
+          nextSrc = $(this).attr('src').replace( currentNum, nextNum);
+          $(this).attr('src', nextSrc ).bind('onreadystatechange load', function(){
+             if (this.complete) $(this).fadeIn(500);
+          });
+      });   
+    };
+
+} /* END PHOTOGRAPHYFEATURES*/
+
+// run if on photo page
+if ( $('#photog-body').length ) { 
+    photographyFeatures();
+}
 
 
 
